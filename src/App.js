8d6e0d39205324895ec2650fbc61c2e5c7cb8932
dmitry <lepsky.dmitry@gmail.com>
2018-12-19 11:12:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import AppBar from './components/appbar/AppBar'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline>
+                    <AppBar/>
+                </CssBaseline>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
