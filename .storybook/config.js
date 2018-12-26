bf85b4fe4f0e6b12aa9ec4f7f0e45343b8844fcf
dmitry <lepsky.dmitry@gmail.com>
2018-12-26 15:39:25 -0500

    + predefined screens + readme fixed

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,39 @@
-import { configure } from '@storybook/react';
+import {configure} from '@storybook/react';
+import {configureViewport} from '@storybook/addon-viewport';
 
 function loadStories() {
-  require('../src/stories');
+    require('../src/stories');
 }
+
+const newViewports = {
+    desktop: {
+        name: 'Desktop',
+        styles: {
+            width: '1284px'
+        }
+    },
+    tablet2: {
+        name: 'Tablet 2',
+        styles: {
+            width: '1080px'
+        }
+    },
+    tablet1: {
+        name: 'Tablet 1',
+        styles: {
+            width: '600px'
+        }
+    },
+    mobile: {
+        name: 'Mobile',
+        styles: {
+            width: '360px'
+        }
+    }
+};
+
+configureViewport({
+    viewports: newViewports
+});
 
 configure(loadStories, module);
