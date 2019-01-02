f745fd3c91fe5b8d8f5878ce567a6ae7bac26649
dmitry <lepsky.dmitry@gmail.com>
2019-01-02 10:15:19 -0500

    Updates

---
+++ precache-manifest.88db17cbb9a4345fd1ef4ee2e554b99b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3bf159a2d325c08b67e",
+    "url": "/poc/static/css/main.31ad1566.chunk.css"
+  },
+  {
+    "revision": "d3bf159a2d325c08b67e",
+    "url": "/poc/static/js/main.d3bf159a.chunk.js"
+  },
+  {
+    "revision": "38e2aea5df725a849608",
+    "url": "/poc/static/js/1.38e2aea5.chunk.js"
+  },
+  {
+    "revision": "c6b33cc2bac4d739db58",
+    "url": "/poc/static/js/runtime~main.c6b33cc2.js"
+  },
+  {
+    "revision": "7977849ed438b83adc3fcaa679f0bc8a",
+    "url": "/poc/index.html"
+  }
+];
