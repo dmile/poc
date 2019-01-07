bd6238a83c3cd2704e991a3c27bd290e12490200
dmitry <lepsky.dmitry@gmail.com>
2019-01-07 16:41:23 -0500

    Updates

---
+++ precache-manifest.e7613e7391fd08826b54893b1916a27a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e71ab4fe4d9c6ade7d53",
+    "url": "/poc/static/css/main.43d1d5ab.chunk.css"
+  },
+  {
+    "revision": "e71ab4fe4d9c6ade7d53",
+    "url": "/poc/static/js/main.e71ab4fe.chunk.js"
+  },
+  {
+    "revision": "8d70cbc56dc2baa58524",
+    "url": "/poc/static/js/1.8d70cbc5.chunk.js"
+  },
+  {
+    "revision": "c6b33cc2bac4d739db58",
+    "url": "/poc/static/js/runtime~main.c6b33cc2.js"
+  },
+  {
+    "revision": "46e16c23e8bffd3138a2cac0a36fa923",
+    "url": "/poc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/poc/precache-manifest.8eb982c7fee1362e51fbd389d637e898.js"
+  "/poc/precache-manifest.e7613e7391fd08826b54893b1916a27a.js"
 );
 
 workbox.clientsClaim();
